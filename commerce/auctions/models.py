6471be9ab6b63c536965183351691bb07ff8fbd8
tds455 <tds-1993@outlook.com>
2022-03-01 16:01:07 +0900

    Updated models
    Added listing model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,20 @@
+from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class listing(models.Model):
+    userid = models.IntegerField()
+    listingname = models.CharField(max_length=64)
+    value = models.DecimalField(decimal_places=2)
+    enddate = models.DateTimeField
+
+class bids(models.Model):
+    pass
+
+class comments(models.Model):
+    pass
+
